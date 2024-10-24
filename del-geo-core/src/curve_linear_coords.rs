67bf1d31acaa5cb2cf1c88ae296f3a721035d38b
Asuka Minato <i@asukaminato.eu.org>
2024-10-24 17:23:28 +0900

    map

--- del-geo-core/src/curve_linear_coords.rs
+++ del-geo-core/src/curve_linear_coords.rs
@@ -9,21 +9,15 @@ where
     let mut gu = [[T::zero(); 3]; 3];
     crate::vec3::cross_mut(&mut gu[0], &gd[1], &gd[2]);
     let invtmp1 = T::one() / crate::vec3::dot(&gu[0], &gd[0]);
-    gu[0][0] = gu[0][0] * invtmp1;
-    gu[0][1] = gu[0][1] * invtmp1;
-    gu[0][2] = gu[0][2] * invtmp1;
+    gu[0] = gu[0].map(|x| x * invtmp1);
     //
     crate::vec3::cross_mut(&mut gu[1], &gd[2], &gd[0]);
     let invtmp2 = T::one() / crate::vec3::dot(&gu[1], &gd[1]);
-    gu[1][0] = gu[1][0] * invtmp2;
-    gu[1][1] = gu[1][1] * invtmp2;
-    gu[1][2] = gu[1][2] * invtmp2;
+    gu[1] = gu[1].map(|x| x * invtmp2);
     //
     crate::vec3::cross_mut(&mut gu[2], &gd[0], &gd[1]);
     let invtmp3 = T::one() / crate::vec3::dot(&gu[2], &gd[2]);
-    gu[2][0] = gu[2][0] * invtmp3;
-    gu[2][1] = gu[2][1] * invtmp3;
-    gu[2][2] = gu[2][2] * invtmp3;
+    gu[2] = gu[2].map(|x| x * invtmp3);
     gu
 }
 
