ba034ea21033f8dccfc713057cb51fa1636d75f8
Nobyuki Umetani <n.umetani@gmail.com>
2024-11-27 19:21:48 +0900

    refactoring vecn

--- del-geo-core/src/vecn.rs
+++ del-geo-core/src/vecn.rs
@@ -1,20 +1,30 @@
 pub trait Arr<T, const N: usize> {
-    fn add(self, other: T) -> Self;
+    fn add(self, other: &[T; N]) -> Self;
+    fn sub(self, other: &[T; N]) -> Self;
+    fn norm(self) -> T;
 }
 
-impl<T, const N: usize> Arr<[T; N], N> for [T; N]
+impl<T, const N: usize> Arr<T, N> for [T; N]
 where
-    T: std::ops::Add<Output = T> + Copy,
+    T: num_traits::Float + Copy + std::iter::Sum,
 {
-    fn add(self, other: [T; N]) -> Self {
+    fn add(self, other: &[T; N]) -> Self {
         std::array::from_fn(|i| self[i] + other[i])
+    }
+    fn sub(self, other: &[T; N]) -> Self {
+        std::array::from_fn(|i| self[i] - other[i])
+    }
+    fn norm(self) -> T {
+        self.iter().map(|&v| v * v).sum::<T>().sqrt()
     }
 }
 
 #[test]
 fn test_add() {
     // Test with different array sizes
-    assert_eq!([1, 2].add([2, 3]), [3, 5]);
-    assert_eq!([1, 2, 3].add([2, 3, 4]), [3, 5, 7]);
-    assert_eq!([1, 2, 3, 4].add([2, 3, 4, 5]), [3, 5, 7, 9]);
+    assert_eq!([1., 2.].add(&[2., 3.]), [3., 5.]);
+    assert_eq!([1., 2.].sub(&[2., 3.]), [-1., -1.]);
+    assert_eq!([4., 3.].norm(), 5.);
+    // assert_eq!([1, 2, 3].add(&[2, 3, 4]), [3, 5, 7]);
+    // assert_eq!([1, 2, 3, 4].add(&[2, 3, 4, 5]), [3, 5, 7, 9]);
 }
