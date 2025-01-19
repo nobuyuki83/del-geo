b56572ae35b5a9ecc4584801990eff658967b69c
Nobyuki Umetani <n.umetani@gmail.com>
2025-01-20 06:54:46 +0900

    add test for svd differential

--- del-geo-core/src/mat3_row_major.rs
+++ del-geo-core/src/mat3_row_major.rs
@@ -1,4 +1,3 @@
-use crate::vec3::Vec3;
 /// trait for 3x3 matrix in row-major order
 pub trait Mat3RowMajor<Real: num_traits::Float> {
     fn from_identity() -> Self;
@@ -12,7 +11,7 @@ pub trait Mat3RowMajor<Real: num_traits::Float> {
 }
 impl<Real> Mat3RowMajor<Real> for [Real; 9]
 where
-    Real: num_traits::Float + std::ops::AddAssign + std::iter::Sum,
+    Real: num_traits::Float + std::iter::Sum,
 {
     fn from_identity() -> Self {
         from_identity()
@@ -78,13 +77,13 @@ where
 
 pub fn mult_mat_row_major<Real>(a: &[Real; 9], b: &[Real; 9]) -> [Real; 9]
 where
-    Real: num_traits::Float + std::ops::AddAssign,
+    Real: num_traits::Float,
 {
     let mut r = [Real::zero(); 9];
     for i in 0..3 {
         for j in 0..3 {
             for k in 0..3 {
-                r[i * 3 + j] += a[i * 3 + k] * b[k * 3 + j]
+                r[i * 3 + j] = r[i * 3 + j] + a[i * 3 + k] * b[k * 3 + j]
             }
         }
     }
@@ -112,7 +111,10 @@ where
     std::array::from_fn(|i| a[i] * s)
 }
 
-fn sort_eigen(g: &mut [f64; 3], v: &mut [f64; 9]) {
+fn sort_eigen<Real>(g: &mut [Real; 3], v: &mut [Real; 9])
+where
+    Real: num_traits::Float,
+{
     if g[1] > g[0] {
         g.swap(0, 1);
         v.swap(0, 1);
@@ -133,8 +135,13 @@ fn sort_eigen(g: &mut [f64; 3], v: &mut [f64; 9]) {
     }
 }
 
-// m = UGV^T
-pub fn svd(m: &[f64; 9], nitr: usize) -> ([f64; 9], [f64; 9], [f64; 9]) {
+/// m = UGV^T
+pub fn svd<Real>(m: &[Real; 9], nitr: usize) -> ([Real; 9], [Real; 9], [Real; 9])
+where
+    Real: num_traits::Float + std::iter::Sum,
+{
+    let one = Real::one();
+    let zero = Real::zero();
     // M^TM = VGGV^T
     let mtm = [
         m[0] * m[0] + m[3] * m[3] + m[6] * m[6],
@@ -148,7 +155,7 @@ pub fn svd(m: &[f64; 9], nitr: usize) -> ([f64; 9], [f64; 9], [f64; 9]) {
         todo!()
     };
     sort_eigen(&mut lv, &mut v);
-    lv = lv.map(|x| x.clamp(0.0, x));
+    lv = lv.map(|x| x.max(zero));
     let mut g = lv.map(|x| x.sqrt());
 
     let mut u0 = [
@@ -167,24 +174,25 @@ pub fn svd(m: &[f64; 9], nitr: usize) -> ([f64; 9], [f64; 9], [f64; 9]) {
         m[6] * v[2] + m[7] * v[5] + m[8] * v[8],
     ];
 
-    let mut u = [0f64; 9];
-    if v.determinant() < 0. {
+    let mut u = [zero; 9];
+    if v.determinant() < zero {
         // making right hand coordinate
-        v[2] *= -1.; // v[0,2] = v[0*3+2]
-        v[5] *= -1.; // v[1,2] = v[1*3+2]
-        v[2 * 3 + 2] *= -1.;
-        g[2] *= -1.;
-        u[2] *= -1.; // u[0,2] = u[0*3+2]
-        u[5] *= -1.; // u[1,2] = u[1*3+2]
-        u[8] *= -1.; // u[2,2] = u[2*3+2]
+        v[2] = -v[2]; // v[0,2] = v[0*3+2]
+        v[5] = -v[5]; // v[1,2] = v[1*3+2]
+        v[2 * 3 + 2] = -v[2 * 3 + 2];
+        g[2] = -g[2];
+        u[2] = -u[2]; // u[0,2] = u[0*3+2]
+        u[5] = -u[5]; // u[1,2] = u[1*3+2]
+        u[8] = -u[8]; // u[2,2] = u[2*3+2]
     }
 
+    use crate::vec3::Vec3;
     let sql0 = u0.squared_norm();
-    if sql0 > 1.0e-20 {
+    if sql0 > Real::epsilon() {
         u0.normalize_in_place();
         let sql1 = u1.squared_norm();
-        if sql1 < 1.0e-20 {
-            u1 = u0.map(|x| 1.0 - x.abs());
+        if sql1 < Real::epsilon() {
+            u1 = u0.map(|x| one - x.abs());
         } else {
             u1.normalize_in_place();
         }
@@ -194,13 +202,13 @@ pub fn svd(m: &[f64; 9], nitr: usize) -> ([f64; 9], [f64; 9], [f64; 9]) {
         let s2 = u0.cross(&u1);
         let d22 = u2[0] * s2[0] + u2[1] * s2[1] + u2[2] * s2[2];
         u2 = s2;
-        if d22 < 0. {
-            g[2] *= -1.;
+        if d22 < zero {
+            g[2] = -g[2];
         }
     } else {
-        u0 = [1., 0., 0.];
-        u1 = [0., 1., 0.];
-        u2 = [0., 0., 1.];
+        u0 = [one, zero, zero];
+        u1 = [zero, one, zero];
+        u2 = [zero, zero, one];
     }
     u[0] = u0[0];
     u[1] = u1[0];
@@ -211,17 +219,48 @@ pub fn svd(m: &[f64; 9], nitr: usize) -> ([f64; 9], [f64; 9], [f64; 9]) {
     u[6] = u0[2];
     u[7] = u1[2];
     u[8] = u2[2];
-    let s = [g[0], 0.0, 0.0, 0.0, g[1], 0.0, 0.0, 0.0, g[2]];
+    let s = [g[0], zero, zero, zero, g[1], zero, zero, zero, g[2]];
     (u, s, v)
 }
 
-/**
- * Jacobian of singular value decomposition
- * @details this is put in the header because MAT can be Eigen::Matrix3
- * Papadopoulo, Théodore & Lourakis, Manolis. (2000).
- * Estimating the Jacobian of the Singular Value Decomposition
- * : Theory and Applications. 554-570.
- */
+#[test]
+fn test_svd() {
+    use rand::Rng;
+    use rand::SeedableRng;
+    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(0);
+    for _iter in 0..10000 {
+        let m: [f64; 9] = std::array::from_fn(|_| rng.gen::<f64>());
+        let (u, s, v) = svd(&m, 100);
+        {
+            let diff = Mat3RowMajor::transpose(&u)
+                .mult_mat_row_major(&u)
+                .sub(&from_identity())
+                .squared_norm();
+            assert!(diff < 1.0e-29f64, "{}", diff);
+        }
+        {
+            let diff = Mat3RowMajor::transpose(&v)
+                .mult_mat_row_major(&v)
+                .sub(&from_identity())
+                .squared_norm();
+            assert!(diff < 1.0e-29f64, "{}", diff);
+        }
+        {
+            let diff = u
+                .mult_mat_row_major(&s)
+                .mult_mat_row_major(&Mat3RowMajor::transpose(&v))
+                .sub(&m)
+                .squared_norm();
+            assert!(diff < 1.0e-23f64, "{}", diff);
+        }
+    }
+}
+
+/// Jacobian of singular value decomposition
+///
+/// Papadopoulo, Théodore & Lourakis, Manolis. (2000). "
+/// Estimating the Jacobian of the Singular Value Decomposition"
+/// Theory and Applications. 554-570.
 pub fn svd_differential(u: [f64; 9], s: [f64; 9], v: [f64; 9]) -> [[[f64; 3]; 3]; 9] {
     let inv_mat2 = |mut a0: f64, a1: f64| -> (f64, f64) {
         if (a0 - a1).abs() < 1.0e-6 {
@@ -236,28 +275,106 @@ pub fn svd_differential(u: [f64; 9], s: [f64; 9], v: [f64; 9]) -> [[[f64; 3]; 3]
     let ai2 = inv_mat2(s[0], s[4]);
 
     let mut diff = [[[0.0; 3]; 3]; 9];
-    for i in 0..3 {
-        for j in 0..3 {
-            // dSdu
-            diff[3][i][j] = u[3 * i] * v[3 * j];
-            diff[4][i][j] = u[3 * i + 1] * v[3 * j + 1];
-            diff[5][i][j] = u[3 * i + 2] * v[3 * j + 2];
-            {
-                let b0 = [-u[3 * i + 2] * v[3 * j + 1], u[3 * i + 1] * v[3 * j + 2]];
-                diff[0][i][j] = b0[0] * ai0.0 + b0[1] * ai0.1;
-                diff[6][i][j] = -b0[0] * ai0.1 - b0[1] * ai0.0;
-            }
-            {
-                let b1 = [-u[3 * i] * v[3 * j + 2], u[3 * i + 2] * v[3 * j]];
-                diff[1][i][j] = b1[0] * ai1.0 + b1[1] * ai1.1;
-                diff[7][i][j] = -b1[0] * ai1.1 - b1[1] * ai1.0;
-            }
-            {
-                let b2 = [-u[3 * i + 1] * v[3 * j], u[3 * i] * v[3 * j + 1]];
-                diff[2][i][j] = b2[0] * ai2.0 + b2[1] * ai2.1;
-                diff[8][i][j] = -b2[0] * ai2.1 - b2[1] * ai2.0;
-            }
+    for (i, j) in itertools::iproduct!(0..3, 0..3) {
+        // dSdu
+        diff[3][i][j] = u[3 * i] * v[3 * j];
+        diff[4][i][j] = u[3 * i + 1] * v[3 * j + 1];
+        diff[5][i][j] = u[3 * i + 2] * v[3 * j + 2];
+        {
+            let b0 = [-u[3 * i + 2] * v[3 * j + 1], u[3 * i + 1] * v[3 * j + 2]];
+            diff[0][i][j] = b0[0] * ai0.0 + b0[1] * ai0.1;
+            diff[6][i][j] = -b0[0] * ai0.1 - b0[1] * ai0.0;
+        }
+        {
+            let b1 = [-u[3 * i] * v[3 * j + 2], u[3 * i + 2] * v[3 * j]];
+            diff[1][i][j] = b1[0] * ai1.0 + b1[1] * ai1.1;
+            diff[7][i][j] = -b1[0] * ai1.1 - b1[1] * ai1.0;
+        }
+        {
+            let b2 = [-u[3 * i + 1] * v[3 * j], u[3 * i] * v[3 * j + 1]];
+            diff[2][i][j] = b2[0] * ai2.0 + b2[1] * ai2.1;
+            diff[8][i][j] = -b2[0] * ai2.1 - b2[1] * ai2.0;
         }
     }
     diff
+}
+
+#[test]
+fn test_svd_differential() {
+    use rand::Rng;
+    use rand::SeedableRng;
+    let mut rng = rand_chacha::ChaCha8Rng::seed_from_u64(0);
+    let eps = 1.0e-6;
+    for _iter in 0..100 {
+        let m0: [f64; 9] = std::array::from_fn(|_| rng.gen::<f64>());
+        let (u0, s0, v0) = svd(&m0, 100);
+        let diff = svd_differential(u0, s0, v0);
+        for (i, j) in itertools::iproduct!(0..3, 0..3) {
+            let m1 = {
+                let mut m1 = m0;
+                m1[i * 3 + j] += eps;
+                m1
+            };
+            let (u1, s1, v1) = svd(&m1, 100);
+            {
+                let du = Mat3RowMajor::transpose(&u0)
+                    .mult_mat_row_major(&u1.sub(&u0))
+                    .scale(1. / eps);
+                let v0 = diff[0][i][j];
+                let v1 = diff[1][i][j];
+                let v2 = diff[2][i][j];
+                assert!(
+                    (du[1 * 3 + 2] - v0).abs() < 1.0e-4 * (1.0 + v0.abs()),
+                    "{}",
+                    v0
+                );
+                assert!(
+                    (du[2 * 3 + 0] - v1).abs() < 1.0e-4 * (1.0 + v1.abs()),
+                    "{}",
+                    v1
+                );
+                assert!(
+                    (du[0 * 3 + 1] - v2).abs() < 1.0e-4 * (1.0 + v2.abs()),
+                    "{}",
+                    v2
+                );
+            }
+            {
+                let ds = [
+                    (s1[0 * 3 + 0] - s0[0 * 3 + 0]) / eps,
+                    (s1[1 * 3 + 1] - s0[1 * 3 + 1]) / eps,
+                    (s1[2 * 3 + 2] - s0[2 * 3 + 2]) / eps,
+                ];
+                let v0 = diff[3][i][j];
+                let v1 = diff[4][i][j];
+                let v2 = diff[5][i][j];
+                assert!((ds[0] - v0).abs() < 1.0e-5 * (1.0 + v0.abs()), "{}", v0);
+                assert!((ds[1] - v1).abs() < 1.0e-5 * (1.0 + v1.abs()), "{}", v1);
+                assert!((ds[2] - v2).abs() < 1.0e-5 * (1.0 + v2.abs()), "{}", v2);
+            }
+            {
+                let dv = Mat3RowMajor::transpose(&v0)
+                    .mult_mat_row_major(&v1.sub(&v0))
+                    .scale(1. / eps);
+                let v0 = diff[6][i][j];
+                let v1 = diff[7][i][j];
+                let v2 = diff[8][i][j];
+                assert!(
+                    (dv[1 * 3 + 2] - v0).abs() < 1.0e-4 * (1.0 + v0.abs()),
+                    "{}",
+                    v0
+                );
+                assert!(
+                    (dv[2 * 3 + 0] - v1).abs() < 1.0e-4 * (1.0 + v1.abs()),
+                    "{}",
+                    v1
+                );
+                assert!(
+                    (dv[0 * 3 + 1] - v2).abs() < 1.0e-4 * (1.0 + v2.abs()),
+                    "{}",
+                    v2
+                );
+            }
+        }
+    }
 }
--- del-geo-core/src/mat3_sym.rs
+++ del-geo-core/src/mat3_sym.rs
@@ -9,18 +9,25 @@ where
         + two * (sm[3] * sm[3] + sm[4] * sm[4] + sm[5] * sm[5])
 }
 
-pub fn eigen_decomp(sm: [f64; 6], nitr: usize) -> Option<([f64; 9], [f64; 3])> {
-    let mut u = [0f64; 9];
+pub fn eigen_decomp<Real>(sm: [Real; 6], nitr: usize) -> Option<([Real; 9], [Real; 3])>
+where
+    Real: num_traits::Float,
+{
+    let zero = Real::zero();
+    let one = Real::one();
+    let two = one + one;
+    let half = one / two;
+    let mut u = [zero; 9];
     // initialize u as identity matrix
-    u[0] = 1.0;
-    u[4] = 1.0;
-    u[8] = 1.0;
+    u[0] = one;
+    u[4] = one;
+    u[8] = one;
     let dnrm = squared_norm(&sm);
-    if dnrm < 1.0e-30 {
+    if dnrm < Real::epsilon() {
         return None;
     } // this matrix is too small
     let scale = dnrm.sqrt();
-    let invscl = 1.0 / scale;
+    let invscl = one / scale;
     let mut sms = sm.map(|x| x * invscl);
 
     for _itr in 0..nitr {
@@ -31,12 +38,12 @@ pub fn eigen_decomp(sm: [f64; 6], nitr: usize) -> Option<([f64; 9], [f64; 3])> {
         let a01 = sms[5].abs();
         if a12 >= a20 && a12 >= a01 {
             // when a12 sms[3] is the biggest
-            let t = 0.5 * (2. * m[3]).atan2(m[2] - m[1]);
+            let t = half * (two * m[3]).atan2(m[2] - m[1]);
             let ct = t.cos();
             let st = t.sin();
-            sms[1] = ct * ct * m[1] + st * st * m[2] - 2. * st * ct * m[3];
-            sms[2] = ct * ct * m[2] + st * st * m[1] + 2. * st * ct * m[3];
-            sms[3] = 0.; // (ct*ct-st*st)*m[3]+st*ct*(m[1]-m[2]);
+            sms[1] = ct * ct * m[1] + st * st * m[2] - two * st * ct * m[3];
+            sms[2] = ct * ct * m[2] + st * st * m[1] + two * st * ct * m[3];
+            sms[3] = zero; // (ct*ct-st*st)*m[3]+st*ct*(m[1]-m[2]);
             sms[4] = st * m[5] + ct * m[4];
             sms[5] = ct * m[5] - st * m[4];
             //
@@ -49,13 +56,13 @@ pub fn eigen_decomp(sm: [f64; 6], nitr: usize) -> Option<([f64; 9], [f64; 3])> {
         } else if a20 >= a01 && a20 >= a12 {
             // when a20 sms[4] is the biggest
             // the above condition statement shoud pass exactly once for each iteration.
-            let t = 0.5 * (2. * m[4]).atan2(m[2] - m[0]);
+            let t = half * (two * m[4]).atan2(m[2] - m[0]);
             let ct = t.cos();
             let st = t.sin();
-            sms[0] = ct * ct * m[0] + st * st * m[2] - 2. * st * ct * m[4];
-            sms[2] = ct * ct * m[2] + st * st * m[0] + 2. * st * ct * m[4];
+            sms[0] = ct * ct * m[0] + st * st * m[2] - two * st * ct * m[4];
+            sms[2] = ct * ct * m[2] + st * st * m[0] + two * st * ct * m[4];
             sms[3] = st * m[5] + ct * m[3];
-            sms[4] = 0.; // (ct*ct-st*st)*m[4]+st*ct*(m[0]-m[2]);
+            sms[4] = zero; // (ct*ct-st*st)*m[4]+st*ct*(m[0]-m[2]);
             sms[5] = ct * m[5] - st * m[3];
             //
             u[0] = ct * v[0] - st * v[2];
@@ -67,15 +74,15 @@ pub fn eigen_decomp(sm: [f64; 6], nitr: usize) -> Option<([f64; 9], [f64; 3])> {
         } else {
             // when a01 sms[5] is the biggest
             // the condition statement shoud pass exactly once for each iteration.
-            let t = 0.5 * (2. * m[5]).atan2(m[1] - m[0]);
+            let t = half * (two * m[5]).atan2(m[1] - m[0]);
             let ct = t.cos();
             let st = t.sin();
-            sms[0] = ct * ct * m[0] + st * st * m[1] - 2. * st * ct * m[5];
-            sms[1] = ct * ct * m[1] + st * st * m[0] + 2. * st * ct * m[5];
+            sms[0] = ct * ct * m[0] + st * st * m[1] - two * st * ct * m[5];
+            sms[1] = ct * ct * m[1] + st * st * m[0] + two * st * ct * m[5];
             sms[3] = st * m[4] + ct * m[3];
             sms[4] = ct * m[4] - st * m[3];
-            sms[5] = 0.; // (ct*ct-st*st)*m[5]+st*ct*(m[0]-m[1]);
-                         //
+            sms[5] = zero; // (ct*ct-st*st)*m[5]+st*ct*(m[0]-m[1]);
+                           //
             u[0] = ct * v[0] - st * v[1];
             u[1] = st * v[0] + ct * v[1];
             u[3] = ct * v[3] - st * v[4];
@@ -89,7 +96,7 @@ pub fn eigen_decomp(sm: [f64; 6], nitr: usize) -> Option<([f64; 9], [f64; 3])> {
 }
 
 #[test]
-fn hoge() {
+fn test_eigen_decomp() {
     use crate::mat3_row_major::Mat3RowMajor;
     use rand::Rng;
     use rand::SeedableRng;
