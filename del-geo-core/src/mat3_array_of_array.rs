1a7f320ca2bfd2b1616f421d848728ab9c5be0d9
Asuka Minato <i@asukaminato.eu.org>
2025-01-20 19:14:29 +0900

    put ref on signature

--- del-geo-core/src/mat3_array_of_array.rs
+++ del-geo-core/src/mat3_array_of_array.rs
@@ -4,23 +4,23 @@ pub trait Mat3ArrayOfArray<Real>
 where
     Self: Sized,
 {
-    fn det_inv(self) -> (Real, Self);
-    fn inverse(self) -> Self;
-    fn matmul(self, b: &Self) -> Self;
+    fn det_inv(&self) -> (Real, Self);
+    fn inverse(&self) -> Self;
+    fn matmul(&self, b: &Self) -> Self;
 }
 
 impl<Real> Mat3ArrayOfArray<Real> for [[Real; 3]; 3]
 where
     Real: num_traits::Float,
 {
-    fn det_inv(self) -> (Real, Self) {
-        det_inv(&self)
+    fn det_inv(&self) -> (Real, Self) {
+        det_inv(self)
     }
-    fn inverse(self) -> Self {
-        inverse(&self)
+    fn inverse(&self) -> Self {
+        inverse(self)
     }
-    fn matmul(self, b: &Self) -> Self {
-        matmul(&self, b)
+    fn matmul(&self, b: &Self) -> Self {
+        matmul(self, b)
     }
 }
 
@@ -58,30 +58,23 @@ pub fn inverse<T>(a: &[[T; 3]; 3]) -> [[T; 3]; 3]
 where
     T: num_traits::Float,
 {
-    det_inv(a).1
+    a.det_inv().1
 }
 
 pub fn matmul<T>(a: &[[T; 3]; 3], b: &[[T; 3]; 3]) -> [[T; 3]; 3]
 where
     T: num_traits::Float,
 {
-    [
-        [
-            a[0][0] * b[0][0] + a[0][1] * b[1][0] + a[0][2] * b[2][0],
-            a[0][0] * b[0][1] + a[0][1] * b[1][1] + a[0][2] * b[2][1],
-            a[0][0] * b[0][2] + a[0][1] * b[1][2] + a[0][2] * b[2][2],
-        ],
-        [
-            a[1][0] * b[0][0] + a[1][1] * b[1][0] + a[1][2] * b[2][0],
-            a[1][0] * b[0][1] + a[1][1] * b[1][1] + a[1][2] * b[2][1],
-            a[1][0] * b[0][2] + a[1][1] * b[1][2] + a[1][2] * b[2][2],
-        ],
-        [
-            a[2][0] * b[0][0] + a[2][1] * b[1][0] + a[2][2] * b[2][0],
-            a[2][0] * b[0][1] + a[2][1] * b[1][1] + a[2][2] * b[2][1],
-            a[2][0] * b[0][2] + a[2][1] * b[1][2] + a[2][2] * b[2][2],
-        ],
-    ]
+    let mut result = [[T::zero(); 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            #[allow(clippy::needless_range_loop)]
+            for k in 0..3 {
+                result[i][j] = result[i][j] + a[i][k] * b[k][j];
+            }
+        }
+    }
+    result
 }
 
 #[test]
