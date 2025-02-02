2bde70038ecf1e6e02e646a050bc6b1a49447dce
Asuka Minato <i@asukaminato.eu.org>
2025-02-02 23:12:06 +0900

    now almost only Float

--- del-geo-core/src/uvec3.rs
+++ del-geo-core/src/uvec3.rs
@@ -1,43 +1,66 @@
 //! methods for unit 3D vector
-pub trait UVec3 {
-    fn map_to_unit2_octahedron(&self) -> [f32; 2];
-    fn map_to_unit2_equal_area(&self) -> [f32; 2];
+
+use num_traits::AsPrimitive;
+pub trait UVec3<Real> {
+    fn map_to_unit2_octahedron(&self) -> [Real; 2];
+    fn map_to_unit2_equal_area(&self) -> [Real; 2];
 }
-impl UVec3 for [f32; 3] {
-    fn map_to_unit2_octahedron(&self) -> [f32; 2] {
+impl<Real> UVec3<Real> for [Real; 3]
+where
+    Real: num_traits::Float + 'static,
+    f64: AsPrimitive<Real>,
+{
+    fn map_to_unit2_octahedron(&self) -> [Real; 2] {
         map_to_unit2_octahedron(self)
     }
-    fn map_to_unit2_equal_area(&self) -> [f32; 2] {
+    fn map_to_unit2_equal_area(&self) -> [Real; 2] {
         map_to_unit2_equal_area(self)
     }
 }
 
-pub fn map_to_unit2_octahedron(dir: &[f32; 3]) -> [f32; 2] {
+pub fn map_to_unit2_octahedron<Real>(dir: &[Real; 3]) -> [Real; 2]
+where
+    Real: num_traits::Float,
+{
+    let one = Real::one();
+    let half = one / (one + one);
+    let zero = Real::zero();
     let n = dir[0].abs() + dir[1].abs() + dir[2].abs();
     let oct = [dir[0] / n, dir[1] / n];
-    let oct = if dir[2] < 0. {
+    let oct = if dir[2] < zero {
         [
-            (1. - oct[1].abs()) * oct[0].signum(),
-            (1. - oct[0].abs()) * oct[1].signum(),
+            (one - oct[1].abs()) * oct[0].signum(),
+            (one - oct[0].abs()) * oct[1].signum(),
         ]
     } else {
         oct
     };
-    [oct[0] * 0.5 + 0.5, oct[1] * 0.5 + 0.5]
+    [oct[0] * half + half, oct[1] * half + half]
 }
 
 /// <https://github.com/mmp/pbrt-v4/blob/1ae72cfa7344e79a7815a21ed3da746cdccee59b/src/pbrt/util/math.cpp#L317>
-pub fn map_to_unit2_equal_area(d: &[f32; 3]) -> [f32; 2] {
+pub fn map_to_unit2_equal_area<Real>(d: &[Real; 3]) -> [Real; 2]
+where
+    Real: num_traits::Float + 'static,
+    f64: AsPrimitive<Real>,
+{
+    let one = Real::one();
+    let zero = Real::zero();
+    let half = one / (one + one);
     let x = d[0].abs();
     let y = d[1].abs();
     let z = d[2].abs();
-    let r = (1. - z).sqrt();
+    let r = (one - z).sqrt();
     let phi = y.atan2(x);
-    let phi = phi * std::f32::consts::FRAC_2_PI;
+    let phi = phi * std::f64::consts::FRAC_2_PI.as_();
     let v = phi * r;
     let u = r - v;
-    let (u, v) = if d[2] < 0. { (1. - v, 1. - u) } else { (u, v) };
+    let (u, v) = if d[2] < zero {
+        (one - v, one - u)
+    } else {
+        (u, v)
+    };
     let u = u.copysign(d[0]);
     let v = v.copysign(-d[1]);
-    [u * 0.5 + 0.5, v * 0.5 + 0.5]
+    [u * half + half, v * half + half]
 }
